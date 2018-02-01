37d75020f06f469c9b3b3a5660c572402e2deacd
Striver-zw <zw@zw-PC>
2018-02-01 14:01:20 +0800

    初始化

---
+++ src/main/java/org/singleton/aibaidu/ZAibaiduApplication.java
@@ -0,0 +1,25 @@
+package org.singleton.aibaidu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+  * @className: ZAibaiduApplication
+  * @description:
+  * @author: singleton-zw
+  * @createDate: 2018年2月1日-下午1:56:39
+  * @updateAuthor:
+  * @updateDate:
+  * @updateDesc:
+  * @version: v1.2.0
+  * @Copyright (c)-2018
+ */
+@SpringBootApplication
+public class ZAibaiduApplication {
+
+	public static void main(String[] args) {
+		 SpringApplication.run(ZAibaiduApplication.class, args);
+	}
+
+}
